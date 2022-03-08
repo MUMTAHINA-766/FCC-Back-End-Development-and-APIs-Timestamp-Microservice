a03fbc374a2039017f66717df1009687ae5838a2
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-03-08 07:39:35 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# FCC-Back-End-Development-and-APIs-Timestamp-Microservice
+
+# [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
---
+++ server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/", function (req, res) {
+  let date = new Date();
+  res.json({
+    unix: date.valueOf(),
+    utc:  date.toUTCString()
+  })
+});
+
+
+app.get('/api/:date', function(req, res){
+
+  let date_params = req.params.date;  
+
+  if(date_params.match(/\d{5,}/)){
+    date_params = +date_params
+  }
+
+  let date = new Date(date_params);
+
+  if(date.toString() === 'Invalid Date'){
+    res.status(400).json({error: 'Invalid Date'});
+  }
+  res.json({unix: date.valueOf(), utc: date.toUTCString()});
+
+})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
